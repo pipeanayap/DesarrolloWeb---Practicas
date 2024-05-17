d97fa8faab241573ff5c683c18b11959514d4233
Luis Felipe Anaya <luisfelipeanayaparada@gmail.com>
2024-05-16 22:51:45 -0600

    Task list and index

--- Practica 21/js/index.js
+++ Practica 21/js/index.js
@@ -1,7 +1,11 @@
 let formulario = document.getElementById('formularioAgregar');
+let formularioEditar = document.getElementById('formularioEditar');
 let nombre = document.getElementById('nombre');
+let nombreEditar = document.getElementById('nombreEditar');
 let fecha = document.getElementById('fecha');
+let fechaEditar = document.getElementById('fechaEditar');
 let descripcion = document.getElementById('descripcion');
+let descripcionEditar = document.getElementById('descripcionEditar');
 let listaTareas = document.getElementById('listaTareas');
 let btnGuardar = document.getElementById('btnGuardar');
 let tareas = [
@@ -28,14 +32,18 @@ formulario.addEventListener("submit", (e) => {
     mostrarTareas();
     cerrarModal();
     formulario.reset()
-    
 });
+
 
 let cerrarModal = () =>{
     btnGuardar.setAttribute("data-bs-dismiss","modal");
     btnGuardar.click()
 }
 
+let cerrarModal2 = () =>{
+    btnGuardarEditar.setAttribute("data-bs-dismiss","modal");
+    btnGuardarEditar.click()
+}
 
 let agregarDatos = () => {
     console.log('Entro a la funcion agregarDatos');
@@ -49,7 +57,6 @@ let agregarDatos = () => {
 }
 
 let mostrarTareas = () => {
-    
     listaTareas.innerHTML = "";
 
     tareas.forEach((tarea, indice) => {
@@ -65,12 +72,43 @@ let mostrarTareas = () => {
                     <strong>${tarea.descripcion}</strong>
                 </div>
                 <div class="col-12 col-md-3 border p-3  text-center">
-                    <button class="btn btn-outline-primary"><i class="bi bi-pencil-fill"></i>&nbsp;&nbsp;Editar</button>
-                    <button class="btn btn-outline-secondary"><i class="bi bi-trash3-fill"></i>&nbsp;Borrar</button>
+                    <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#editarModal" onClick="editarTarea(${indice})"><i class="bi bi-pencil-fill"></i>&nbsp;&nbsp;Editar</button>
+                    <button class="btn btn-outline-secondary" onClick="borrarTarea(this,${indice})"><i class="bi bi-trash3-fill"></i>&nbsp;Borrar</button>
                 </div>
             </div>
         `
     });
 }
 
-mostrarTareas()
+borrarTarea = (boton,indice) =>{
+    if(confirm("¿Estas seguro de borrarlo?")){
+        if(confirm("¿Estas muy seguro, camarada?")){
+            boton.parentElement.parentElement.remove(); 
+            tareas.splice(indice,1);
+        }else{
+            alert("okei, va");
+        }
+    }else{
+        alert("okei, va");
+    }
+}
+
+editarTarea = (indice) => {
+    nombreEditar.value = tareas[indice].nombre;
+    fechaEditar.value = tareas[indice].fecha;
+    descripcionEditar.value = tareas[indice].descripcion;
+
+    formularioEditar.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        tareas[indice].nombre = nombreEditar.value;
+        tareas[indice].fecha = fechaEditar.value;
+        tareas[indice].descripcion = descripcionEditar.value;
+
+        mostrarTareas();
+        cerrarModal2();
+    });
+}
+
+
+mostrarTareas();
